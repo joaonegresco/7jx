2f7ccd89ce17f9d7d0dc105b9bc806086c71be1b
joaonegresco <92122150+joaonegresco@users.noreply.github.com>
2026-03-26 09:01:05 -0300

    up

--- atvd.4/list.js
+++ atvd.4/list.js
@@ -28,44 +28,58 @@ const pilha =
 
 ];
     pilha.push("O Alquimista");
+
     const livro = pilha.pop();
+
     console.log(livro);
-// 1.4 Buscar se existe
+
+
 const temLaranja = frutas.includes("Laranja");
+
 const temManga = frutas.includes("Manga");
+
 console.log(temLaranja);
+
 console.log(temManga);
 
-// 2. Sets (Conjuntos)
-// 2.1 Adicionando duplicados
+os
 const matriculas = new Set ([1001, 1002, 1005, 1009, 1002]);
+
 console.log([...matriculas]);
-// 2.2 Adicionar item
+
+
 const cores = new Set (["Verde", "Azul"]);
+
 cores.add("Vermelho");
 cores.add("Branco");
 cores.add("Preto");
+
 console.log([...cores]);
-// 2.3 Buscar se existe
+
 const temVerde = cores.has("Verde");
 const temAmarelo = cores.has("Amarelo");
 console.log(temVerde);
+
 console.log(temAmarelo);
-// 2.4 Converter array em set 
+
 const convidados = ["Bruno", "Henrique", "Juliana", 
     "Carol", "Juliana", "Breno", "Henrique"];
+
 const convidados_unicos = new Set(convidados);
+
 console.log([...convidados_unicos]);
 
-// 3. Map (Mapas)
-// 3.1 Cadastro de Preços
 const produtos = new Map();
 produtos.set("Mouse", 50.00);
 produtos.set("Teclado", 120.00);
-// 3.2 Buscando um Valor
+
+
 valorMouse = produtos.get("Mouse");
+
 console.log(valorMouse);
+
 valorTeclado = produtos.get("Teclado");
+
 console.log(valorTeclado);
 
 
